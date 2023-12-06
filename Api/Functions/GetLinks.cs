18923d34b29dc5a0d481414ca1908cc7a3ecc169
yavaka <yavakasolutions@gmail.com>
2023-12-06 23:03:15 +0000

    Added GetLinks Function & connection to cosmos db

---
+++ Api/Functions/GetLinks.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using BlazorApp.Shared;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Functions;
+
+public class GetLinks(ILoggerFactory loggerFactory)
+{
+    private readonly ILogger _logger = loggerFactory.CreateLogger<GetLinks>();
+
+    [Function("GetLinks")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "links/{vanityUrl}")] HttpRequestData req,
+        [CosmosDBInput(
+            databaseName: "blazor-swa",
+            collectionName: "linkbundles",
+            ConnectionStringSetting = "CosmosDbConnectionString",
+            SqlQuery ="SELECT * FROM c WHERE c.vanityUrl = {vanityUrl}",
+            PartitionKey = "{vanityUrl}")] IEnumerable<LinkBundle> linkItem)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(linkItem.First());
+
+        return response;
+    }
+}
---
+++ Shared/LinkBundle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorApp.Shared
+{
+    public class LinkBundle
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string VanityUrl { get; set; }
+        public string Description { get; set; }
+
+        public List<Link> Links { get; set; }
+    }
+}
